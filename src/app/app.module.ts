3ec0c81aed395711cb622d1efbe2e8312610a4b8
webcat12345 <webcat91@gmail.com>
2017-04-26 11:45:32 +0200

    issue #1 - set startup project using angular material and animation module * install hammerjs * include material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { SharedMaterialModuleModule } from './shared/shared-material-module/shared-material-module.module';
