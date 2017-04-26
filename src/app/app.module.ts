b77d7778d581f5e0a4ec35aba8933ed7e103a4a4
webcat12345 <webcat91@gmail.com>
2017-04-26 15:41:20 +0200

    issue #1 - set startup project using angular material and animation module * import angular flexlayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedMaterialModuleModule } from './shared/shared-material-module/shared-material-module.module';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
-import { SharedMaterialModuleModule } from './shared/shared-material-module/shared-material-module.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { SharedMaterialModuleModule } from './shared/shared-material-module/shar
     NoopAnimationsModule,
     FormsModule,
     HttpModule,
-    SharedMaterialModuleModule
+    SharedMaterialModuleModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
