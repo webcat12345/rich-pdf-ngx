dc451130be4a44e5fc83421bc2d17bec530fe275
webcat12345 <webcat91@gmail.com>
2017-05-06 05:08:11 +0200

    express server redirect issue

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-  const express = require('express');
+const express = require('express');
+const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
